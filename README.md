227108f10d8087992621fe1af70278726bae38a6
Nikita <nikita@intspirit.com>
2016-03-17 16:23:39 +0300

    added heroku domain address

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is the Intspirit test implementation
+You can see implementation of this test on https://frozen-dusk-64465.herokuapp.com.
--- public/js/components/common/network/api.js
+++ public/js/components/common/network/api.js
@@ -1,7 +1,7 @@
 define(function (require) {
     var $ = require('jquery'),
         ajax = require('./ajax'),
-        baseUrl = 'http://localhost:3000';
+        baseUrl = 'https://frozen-dusk-64465.herokuapp.com';
 
     var that = {};
 
