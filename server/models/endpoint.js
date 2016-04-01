83a78dc96d478f7cc68187d02f7218bc1c97b72e
Nikita <nikita@intspirit.com>
2016-04-01 11:06:33 +0300

    added response_codes endpoint

--- server/models/endpoint.js
+++ server/models/endpoint.js
@@ -1,3 +1,27 @@
 module.exports = function(Endpoint) {
 
+  Endpoint.response_codes = function (cb) {
+    var rand = randomInteger(0, 1);
+    var response;
+    if (rand) {
+      response = { result: true, text: 'Success!' };
+    } else {
+      response = { result: false, text: 'Failure!' };
+    }
+    cb(null, response);
+  };
+
+  Endpoint.remoteMethod(
+    'response_codes',
+    {
+      http: {path: '/response_codes', verb: 'get'},
+      returns: {arg: 'response', type: 'json'}
+    }
+  );
+
+  function randomInteger(min, max) {
+    var rand = min + Math.random() * (max - min);
+    rand = Math.round(rand);
+    return rand;
+  }
 };
